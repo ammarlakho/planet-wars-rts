1d5878aa5ef9ceb57070483bc4c66f0f14b208fc
SimonLucas <simonmarklucas@gmail.com>
2025-02-27 09:59:44 +0000

    Update Vec2d.kt
    Added a Vec2d class

--- app/src/main/kotlin/util/Vec2d.kt
+++ app/src/main/kotlin/util/Vec2d.kt
@@ -2,13 +2,11 @@ package util
 
 import java.lang.Math.sqrt
 
-data class Vec2d (val x: Double, val y: Double) {
+data class Vec2d(val x: Double, val y: Double) {
 
-//    operator plus(v: Vec2) = Vec2d(x+ v.x, y+v.y)
+    operator fun plus(v: Vec2d) = Vec2d(x + v.x, y + v.y)
 
-    fun plus(v: Vec2d) = Vec2d(x+v.x, y+v.y)
+    
 
-    fun magnitude() = sqrt(x*x + y*y)
-
+    fun magnitude() = sqrt(x * x + y * y)
 }
-
