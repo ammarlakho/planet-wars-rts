58253ce2a8ffc5ff50323c8451e82b12c745e7dd
SimonLucas <simonmarklucas@gmail.com>
2025-03-15 12:10:15 +0000

    Added classes for partially observable version of the game
    Main points:
    
    Observation includes the aspects of a game state that can be observed by a specified set of players
    
    GameStateReconstructor takes in an observation and distributions over the unknown quantities, samples from the distributions and returns a GameState object, which can then be used with the ForwardModel for statistical planning algorithms.
    
    Updated AgentInterfaces for partially observable version of game

--- app/src/main/kotlin/games/planetwars/agents/AgentInterfaces.kt
+++ app/src/main/kotlin/games/planetwars/agents/AgentInterfaces.kt
@@ -2,18 +2,17 @@ package games.planetwars.agents
 
 import games.planetwars.core.GameParams
 import games.planetwars.core.GameState
+import games.planetwars.core.Observation
 import games.planetwars.core.Player
 
-class AgentInterfaces {
-}
-
 /*
-    * This interface defines the methods that an agent must implement to play the game
+    * This interface defines the methods that an agent must implement to play the fully observable game
  */
+
 interface PlanetWarsAgent {
     fun getAction(gameState: GameState): Action
     fun getAgentType(): String
-    fun prepareToPlayAs(player: Player, params: GameParams): PlanetWarsAgent
+    fun prepareToPlayAs(player: Player, params: GameParams, opponent: Player? = null): PlanetWarsAgent
 
     // this is provided as a default implementation, but can be overridden if needed
     fun processGameOver(finalState: GameState) {}
@@ -28,7 +27,39 @@ abstract class PlanetWarsPlayer : PlanetWarsAgent {
     protected var player: Player = Player.Neutral
     protected var params: GameParams = GameParams()
 
-    override fun prepareToPlayAs(player: Player, params: GameParams): PlanetWarsAgent {
+    override fun prepareToPlayAs(player: Player, params: GameParams, opponent: Player?): PlanetWarsAgent {
+        this.player = player
+        this.params = params
+        return this
+    }
+}
+
+
+/*
+    * This interface defines the methods that an agent must implement to play the partially observable game
+ */
+
+interface PartialObservationAgent {
+    fun getAction(observation: Observation): Action
+    fun getAgentType(): String
+    fun prepareToPlayAs(player: Player, params: GameParams, opponent: Player? = null): PartialObservationAgent
+
+    // this is provided as a default implementation, but can be overridden if needed
+    // note that the final state is fully observable, so the agent can use this to learn from the final state
+    fun processGameOver(finalState: GameState) {}
+
+}
+
+/*
+    * Kotlin only has single code inheritance, so we use an abstract class to provide a default implementation
+    * of prepareToPlayAs - this is useful because many agents will need to know which player they are playing as
+    * and may need other resets or initializations prior to playing
+ */
+abstract class PartialObservationPlayer : PartialObservationAgent {
+    protected var player: Player = Player.Neutral
+    protected var params: GameParams = GameParams()
+
+    override fun prepareToPlayAs(player: Player, params: GameParams, opponent: Player?): PartialObservationAgent {
         this.player = player
         this.params = params
         return this
