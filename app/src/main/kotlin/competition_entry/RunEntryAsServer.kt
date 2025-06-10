638af6c4291228875bfca3d58e4188dd8853adc7
Ammar Lakho <ammar1lakho@gmail.com>
2025-06-10 02:23:07 +0200

    use StrategicHeuristicAgent in GameAgentServer

--- app/src/main/kotlin/competition_entry/RunEntryAsServer.kt
+++ app/src/main/kotlin/competition_entry/RunEntryAsServer.kt
@@ -1,9 +1,10 @@
 package competition_entry
 
 import games.planetwars.agents.random.CarefulRandomAgent
+import games.planetwars.agents.strategic.StrategicHeuristicAgent
 import json_rmi.GameAgentServer
 
 fun main() {
-    val server = GameAgentServer(port = 8080, agentClass = CarefulRandomAgent::class)
+    val server = GameAgentServer(port = 8080, agentClass = StrategicHeuristicAgent::class)
     server.start(wait = true)
 }
